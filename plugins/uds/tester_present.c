43bb075753f46f4135fc51be6201a1df85bbe05d
tobbe <tobias.rasmusson@mail.com>
2017-02-05 00:16:54 +0100

    use response variable

--- plugins/uds/tester_present.c
+++ plugins/uds/tester_present.c
@@ -9,6 +9,7 @@
 
 static int hf_tp_type = -1;
 static int hf_suppress_pos_rsp_msg_indication_bit = -1;
+static int hf_tp_type_response = -1;
 
 static const value_string sub_function[] = {
 	{ 0x0, "zeroSubFunction" },
@@ -44,7 +45,7 @@ gint add_tester_present_response_fields(proto_tree *uds_tree, packet_info *pinfo
 
 	if (uds_tree)
 	{
-		proto_tree_add_item(uds_tree, hf_tp_type, tvb, offset, 1, ENC_BIG_ENDIAN);
+		proto_tree_add_item(uds_tree, hf_tp_type_response, tvb, offset, 1, ENC_BIG_ENDIAN);
 	}
 
 	return offset + 1;
@@ -82,7 +83,7 @@ void proto_register_tester_present_response(gint proto_uds)
 	static hf_register_info hf_tester_present_response[] =
 	{
 		{
-			&hf_tp_type,
+			&hf_tp_type_response,
 			{
 				"Sub Function", "uds.tester_present.subFunction",
 				FT_UINT8, BASE_HEX,
